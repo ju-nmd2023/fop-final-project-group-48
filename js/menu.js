4db0b3fcac4d19188853d7877a8b606c7aa16634
JonatanS23 <149917694+JonatanS23@users.noreply.github.com>
2024-04-12 13:47:59 +0200

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+const difficultyBtn = document.getElementById("difficulty-bttn");
+const infiniteBtn = document.getElementById("infinite-bttn");
+const controlsBtn = document.getElementById("controls-bttn");
+const menu = document.getElementById("menu");
+const menuBtn = document.getElementById("menu-bttn");
+
+difficultyBtn.addEventListener("click", function (event) {
+  console.log("Difficulty Button Clicked!");
+
+  menu.style.display = "none";
+});
+
+infiniteBtn.addEventListener("click", function (event) {
+  console.log("Infinite Button Clicked!");
+
+  menu.style.display = "none";
+});
+
+controlsBtn.addEventListener("click", function (event) {
+  console.log("Controls Button Clicked!");
+
+  menu.style.display = "none";
+});
+
+menuBtn.addEventListener("click", function (event) {
+  console.log("Menu Button Clicked!");
+
+  menu.style.display = "block";
+});
